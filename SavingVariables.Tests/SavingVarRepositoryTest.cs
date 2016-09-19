9a18e1a986db77f7a5beadf9da6e2a3277a0bbc3
David Cramb <david.cramb@gmail.com>
2016-09-18 19:10:51 -0500

    first unit test pre-Moq

---
+++ SavingVariables.Tests/SavingVarRepositoryTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SavingVariables.DAL;
+using System.Linq;
+using System.Collections.Generic;
+using Moq;
+using System.Data.Entity;
+using SavingVariables.Models;
+
+namespace SavingVariables.Tests
+{
+    [TestClass]
+    public class SavingVarRepositoryTest
+    {
+        [TestMethod]
+        public void EnsureCanCreateInstanceOfSavingVar()
+        {
+            SavingVarRepository saved = new SavingVarRepository();
+            Console.WriteLine(saved);
+        }
+    }
+}
